03d4368182add043dcde285e5fc67198cdd584c8
Vaibhav Ghage <78686643+ghagevaibhav@users.noreply.github.com>
2024-06-02 13:30:11 +0530

    Create 344. Reverse String.cpp

---
+++ LeetCode-Daily-June-2024/Day - 2/344. Reverse String.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        // using the inbuilt reverse function, just reverse the char array 
+        reverse(s.begin(), s.end());
+    }
+};
